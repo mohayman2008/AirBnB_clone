1eb9a41e66bf9b71829fa5df85938255d9e15985
Mohamed Ayman <mohayman2007@gmail.com>
2022-11-24 02:07:52 +0000

    Project 0x00. The console - Task 3 - BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""This module contains the difinition of the BaseModel class"""
+
+from datetime import datetime
+from uuid import uuid4
+
+
+class BaseModel:
+    """The base model for all the objects of the app"""
+
+    def __init__(self):
+        """Creates an object of the class"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        print(self.created_at)
+
+    def __str__(self):
+        """Returns the string representation of the object"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """Updates self.updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+        pass
+
+    def to_dict(self):
+        """Returns a dictionary representation of the object"""
+        dictionary = dict(self.__dict__)
+        dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        return dictionary
+    pass
