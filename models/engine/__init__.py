07b1f44a37df07f43ad8aca6aff0eefcb8a0d1f9
Mohamed Ayman <mohayman2007@gmail.com>
2022-11-26 01:52:26 +0000

    Project 0x00. The console - Task 10 - Console 1.0

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -3,7 +3,13 @@
 
 from ..base_model import BaseModel
 from ..user import User
+from ..state import State
+from ..city import City
+from ..amenity import Amenity
+from ..place import Place
+from ..review import Review
 
-classes = {"BaseModel": BaseModel, "User": User}
+classes = {"BaseModel": BaseModel, "User": User, "State": State, "City": City,
+           "Amenity": Amenity, "Place": Place, "Review": Review}
 
 __all__ = ["file_storage"]
